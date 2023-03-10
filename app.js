c4c0a4a2bb74793d951ce1976da3fba96c8115a1
Mohamed Mostafa <Mohamedmsaber@outlook.com>
2023-03-10 02:19:02 +0200

    send email configration

--- app.js
+++ app.js
@@ -1,17 +1,19 @@
 const express = require('express')
 const dbConnection = require('./src/database/dbConnection')
 require('dotenv').config({path:'./config/.env'})
+const morgan = require('morgan');
 const app = express()
 const port = process.env.PORT || 4000
 app.use(express.json())
 const indexRouter = require('./src/index.router')
-const globalMiddleware = require("./src/utils/GolbalMiddleware")
+const globalMiddleware = require("./src/utils/GolbalMiddleware");
+const AppError = require('./src/utils/AppError');
 if (process.env.MODE_ENV === 'devolpment') {
     app.use(morgan('dev'));
 }
 
 
-app.use("api/v1/parent" , indexRouter.parentRouter)
+app.use("/api/v1/parent" , indexRouter.parentRouter)
 app.all('*', (req, res,next) =>{
     next(new AppError(`Route : ${req.originalUrl} not found on Server`, 404));
 })
--- src/components/User/Parent/parent.auth.js
+++ src/components/User/Parent/parent.auth.js
@@ -1,3 +1,4 @@
+const parentModel = require('./parent.model')
 const AppError = require("../../../utils/AppError");
 const { catchAsyncErrors } = require("../../../utils/catchAsync");
 const bcrypt = require('bcrypt');
@@ -6,9 +7,9 @@ const sendEmail = require('../../../utils/sendEmail');
 
 // Sign Up user
 const signup = catchAsyncErrors(async(req , res , next)=>{
-    let user = await userModel.findOne({ email: req.body.email });
+    let user = await parentModel.findOne({ email: req.body.email });
     if (!user) {
-        let newUser = new userModel(req.body);
+        let newUser = new parentModel(req.body);
         await newUser.save();
         const html = `<a href = "${req.protocol}://${req.headers.host}/api/v1/users/confirmEmail/${newUser._id}">Click Here To Confirm Email</a?`;
         sendEmail(newUser.email , html )
@@ -24,7 +25,7 @@ const signup = catchAsyncErrors(async(req , res , next)=>{
 // Confirm Email
 const confirmEmail = catchAsyncErrors(async (req, res , next) => {
     const userId = req.params.id;
-    await userModel.findByIdAndUpdate(userId, { emailConfirmed: true });
+    await parentModel.findByIdAndUpdate(userId, { emailConfirmed: true });
     res.json({ message: "Email Has been Confirmed Successfully", })
 })
 
