a2a5014d68416262f27244d8f96ccf1b73e98da7
Mohamed Mostafa <Mohamedmsaber@outlook.com>
2023-03-12 02:29:04 +0200

    edit parent schema

--- src/components/User/Parent/parent.auth.js
+++ src/components/User/Parent/parent.auth.js
@@ -1,4 +1,4 @@
-const parentModel = require('./parent.model')
+const parentModel = require('../user.model')
 const AppError = require("../../../utils/AppError");
 const { catchAsyncErrors } = require("../../../utils/catchAsync");
 const bcrypt = require('bcrypt');
