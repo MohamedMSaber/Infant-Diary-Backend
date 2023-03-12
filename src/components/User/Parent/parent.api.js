076c244091c7386cd8aed350846c449cc6465451
Mohamed Mostafa <Mohamedmsaber@outlook.com>
2023-03-12 03:02:38 +0200

    test sign in, sign up , confirm email

--- src/components/User/Parent/parent.api.js
+++ src/components/User/Parent/parent.api.js
@@ -1,13 +1,14 @@
-const { signup, confirmEmail } = require('./parent.auth')
+const { signup, confirmEmail, signIn } = require('./parent.auth')
 
 
 const router=require('express').Router()
 
 router.post("/signup",signup)
 router.get("/confirmEmail/:id",confirmEmail)
+router.post("/signIn",signIn)
 // router.get("/refreshEmail/:id",refreshEmail)
 
-// router.post("/login",login)
+
 
 // router.post("/sendCode",sendCode)
 // router.post("/forgetPassword",forgetPassword)
--- src/components/User/Parent/parent.auth.js
+++ src/components/User/Parent/parent.auth.js
@@ -11,7 +11,7 @@ const signup = catchAsyncErrors(async(req , res , next)=>{
     if (!user) {
         let newUser = new parentModel(req.body);
         await newUser.save();
-        const html = `<a href = "${req.protocol}://${req.headers.host}/api/v1/users/confirmEmail/${newUser._id}">Click Here To Confirm Email</a?`;
+        const html = `<a href = "${req.protocol}://${req.headers.host}/api/v1/parent/confirmEmail/${newUser._id}">Click Here To Confirm Email</a?`;
         sendEmail(newUser.email , html )
         console.log(newUser);
         res.status(200).json({ Email:newUser.email , message :  "Sign Up Successfully ...plz confirm your EMAIL..." });
@@ -25,12 +25,27 @@ const signup = catchAsyncErrors(async(req , res , next)=>{
 // Confirm Email
 const confirmEmail = catchAsyncErrors(async (req, res , next) => {
     const userId = req.params.id;
-    await parentModel.findByIdAndUpdate(userId, { emailConfirmed: true });
+    await parentModel.findByIdAndUpdate(userId, { emailConfirm: true });
     res.json({ message: "Email Has been Confirmed Successfully", })
 })
 
+// sign In
+
+const signIn = catchAsyncErrors(async (req, res, next) => {
+    let user = await parentModel.findOne({ email: req.body.email });
+    if (!user || !(await bcrypt.compare(req.body.password, user.password))) {
+        return next(new AppError(`Incorrect Email or Password`, 400));
+    }
+    else if (!user.emailConfirm){
+        return next(new AppError(`First Confirm Your Email...`, 400));
+    }
+    console.log("here");
+    let token = jwt.sign({ name : user.name , userId : user._id }, process.env.JWT_KEY);
+    res.status(200).json({ token})
+})
 
 module.exports = {
     signup,
-    confirmEmail
+    confirmEmail,
+    signIn
 }
