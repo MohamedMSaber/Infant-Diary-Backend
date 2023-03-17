b00dc75f15c731e6607bf9cdfe9f833e98efadf9
fatma-ezzat <70448306+fatma-ezzat@users.noreply.github.com>
2023-03-18 00:13:45 +0200

    Add files via upload

---
+++ src/components/Vaccine/vaccine.model.js
@@ -0,0 +1,14 @@
+const { Schema, model} = require("mongoose");
+const bcrypt = require('bcrypt');
+
+const schema = Schema({
+    name: { type: String, required: [true, 'name is required']},
+    compulsory:{type: String, required:[true, 'you must mention if it is compulsory or not'], enum:['Yes', 'No']},
+    dose:{type: String, required:[true, 'dose is required']},
+    age:{type: Number, required:[true, 'age is required']},
+    sideEffects:{type: String, required:[true, 'side effects is required']},
+    reason:{type: String, required:[true, 'reason is required']},
+},{ timestamps: true })
+
+
+module.exports = model('vaccine' , schema);
