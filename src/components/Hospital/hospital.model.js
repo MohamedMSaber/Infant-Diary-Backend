8f93a3b8600f0e0116ebdf0510647d1f44265159
fatma-ezzat <70448306+fatma-ezzat@users.noreply.github.com>
2023-03-18 00:16:37 +0200

    Add files via upload

---
+++ src/components/Hospital/hospital.model.js
@@ -0,0 +1,20 @@
+const { Schema, model} = require("mongoose");
+const bcrypt = require('bcrypt');
+
+const schema = Schema({
+    name: { type: String, required: [true, 'name is required']},
+    email: { type: String, required: [true, 'email is required'], unique: true},
+    password: { type: String, required: [true, 'password is required']},
+    address: { type: String, required: [true, 'address is required']},
+    city: { type: String, required: [true, 'city is required']},
+    state: { type: String, required: [true,'state is required']},
+    zip: { type: String, required: [true, 'zip is required']},
+    phone: { type: String, required: [true, 'phone number is required']},
+    services:{type: String, required: [true, 'services is required']},
+    link: { type: String, required: [true, 'hospital link is required']},
+    vaccines:[{type:mongoose.Schema.Types.ObjectId,ref:"vaccine"}],
+   
+},{ timestamps: true })
+
+
+module.exports = model('hospital' , schema);
