22660f7a95e2815630a4d6cf674ee64603fd7319
fatma-ezzat <70448306+fatma-ezzat@users.noreply.github.com>
2023-03-18 00:18:19 +0200

    Add files via upload

---
+++ src/components/Clinic/clinic.model.js
@@ -0,0 +1,17 @@
+const { Schema, model} = require("mongoose");
+const bcrypt = require('bcrypt');
+
+const schema = Schema({
+    name: { type: String, required: [true, 'name is required']},
+    email: { type: String, required: [true, 'email is required'], unique: true},
+    password: { type: String, required: [true, 'password is required']},
+    address: { type: String, required: [true, 'address is required']},
+    city: { type: String, required: [true, 'city is required']},
+    state: { type: String, required: [true,'state is required']},
+    zip: { type: String, required: [true, 'zip is required']},
+    phone: { type: String, required: [true, 'phone number is required']},
+    link: { type: String, required: [true, 'hospital link is required']},
+},{ timestamps: true })
+
+
+module.exports = model('clinic' , schema);
