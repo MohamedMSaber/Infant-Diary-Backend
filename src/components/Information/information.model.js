986e06358df389f33cca2835f21c66ab75f65482
fatma-ezzat <70448306+fatma-ezzat@users.noreply.github.com>
2023-03-18 00:20:48 +0200

    Add files via upload

---
+++ src/components/Information/information.model.js
@@ -0,0 +1,13 @@
+const { Schema, model} = require("mongoose");
+const bcrypt = require('bcrypt');
+
+const schema = Schema({
+    topic:{type:String, required:[true, 'topic is required']}, //feeding-sleeping-playing
+    body:{type:String, required:[true, 'body is required']},
+    type:{type:String, required:[true, 'type is required']}, //instructions or article ... 
+    age:{type:Number, required:[true, 'age is required']},
+    link:{type:String},
+    videoLink:{type:String}
+},{ timestamps: true })
+
+module.exports = model('information' , schema);
