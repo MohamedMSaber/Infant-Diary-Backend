c078e87502b9301456b01caaa05c2b3f851a5536
amirafathy-59 <amirafa592001@gmail.com>
2023-03-19 16:22:57 +0000

    add Question Api

--- src/components/User/Admin/admin.api.js
+++ src/components/User/Admin/admin.api.js
@@ -1,7 +1,8 @@
-const { addAnnouncement } = require('./admin.services');
+const { addAnnouncement, addFAQ } = require('./admin.services');
 
 const router=require('express').Router();
 
 router.post("/addAnnouncement",addAnnouncement)
+router.post("/addQuestion",addFAQ)
 
 module.exports=router
--- src/components/User/Admin/admin.services.js
+++ src/components/User/Admin/admin.services.js
@@ -1,5 +1,6 @@
 const { catchAsyncErrors } = require("../../../utils/catchAsync");
 const announcementModel = require("../../Announcement/announcement.model");
+const questionModel = require("../../Question/question.model");
 
 
 
@@ -8,4 +9,11 @@ exports.addAnnouncement = catchAsyncErrors(async (req, res) => {
     let announcement = new announcementModel(req.body);
     await announcement.save();
     res.status(200).json({announcement,message:"You have been added Announcement Successfully..."});
+  });
+
+  exports.addFAQ = catchAsyncErrors(async (req, res) => {
+    const {body,answer,age,virusName}= req.body;
+    let question = new questionModel(req.body);
+    await question.save();
+    res.status(200).json({question,message:"You have been added Question Successfully..."});
   });
