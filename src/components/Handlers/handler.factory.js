bf1a14ec0be351de47e2fcd601439938a76c833e
amirafathy-59 <amirafa592001@gmail.com>
2023-03-19 20:02:34 +0000

    Crud Operations ( Announcement, Question) with Refactor

--- src/components/Handlers/handler.factory.js
+++ src/components/Handlers/handler.factory.js
@@ -1,6 +1,6 @@
 const slugify = require("slugify")
-const AppError = require('../../Utilities/AppError')
-const { catchAsyncErrors } = require('../../Utilities/catchAsync')
+const AppError = require('../../utils/AppError')
+const { catchAsyncErrors } = require('../../utils/catchAsync')
 
 
 const deleteFun = (Model) => {
@@ -13,10 +13,44 @@ const deleteFun = (Model) => {
     res.status(200).json({ message: `document has Been Deleted`  , document});
 })
 }
+const updateFun = (Model) => {
+    return catchAsyncErrors(async (req, res,next) => {
+    const { id } = req.params;
+    let document = await Model.findByIdAndUpdate(id,req.body,{new:true});
+    if (!document) {
+        return next(new AppError(`document Not Found To Update`, 404));
+    }
+    res.status(200).json({ message: `document has Been Updated`  , document});
+})
+}
+
+const getSpecficFun = (Model) => {
+    return catchAsyncErrors(async (req, res,next) => {
+    const { id } = req.params;
+    let document = await Model.find({_id:id});
+    if (!document) {
+        return next(new AppError(`No document Found `, 404));
+    }
+    res.status(200).json({ message: `Document `  , document});
+})
+}
+
+const getAllFun = (Model) => {
+    return catchAsyncErrors(async (req, res,next) => {
+    let documents = await Model.find();
+    if (!documents) {
+        return next(new AppError(`No documents Found `, 404));
+    }
+    res.status(200).json({ message: `Documents `  , documents});
+})
+}
 
 
 
 
 module.exports = {
-    deleteFun
+    deleteFun,
+    updateFun,
+    getAllFun,
+    getSpecficFun
 }
--- src/components/User/Admin/admin.api.js
+++ src/components/User/Admin/admin.api.js
@@ -1,8 +1,15 @@
-const { addAnnouncement, addFAQ } = require('./admin.services');
-
+const { addAnnouncement, addFAQ, updateFAQ, getQuestions, getQuestion, deleteQuestion, updateAnnouncement, deleteAnnouncement, getAnnouncements, getAnnouncement } = require('./admin.services');
 const router=require('express').Router();
 
-router.post("/addAnnouncement",addAnnouncement)
-router.post("/addQuestion",addFAQ)
+router.post("/addAnnouncement",addAnnouncement);
+router.put("/:id/updateAnnouncement",updateAnnouncement);
+router.delete("/:id/deleteAnnouncement",deleteAnnouncement);
+router.get("/announcements",getAnnouncements);
+router.get("/announcements/:id",getAnnouncement);
 
-module.exports=router
+router.post("/addQuestion",addFAQ);
+router.put("/:id/updateQuestion",updateFAQ);
+router.get("/questions",getQuestions);
+router.get("/questions/:id",getQuestion);
+router.delete("/:id/deleteQuestion",deleteQuestion);
+module.exports= router
