a3ec2e4860641b62cee8eac82e8f946315ea3bf8
Mohamed Mostafa <Mohamedmsaber@outlook.com>
2023-03-21 14:43:31 +0200

    edit spacing

--- src/index.router.js
+++ src/index.router.js
@@ -1,13 +1,9 @@
 const authRouter = require("./components/User/Auth/auth.api")
 const parentRouter = require("./components/User/Parent/parent.api")
 const adminRouter = require("./components/User/Admin/admin.api")
-
 const announcementRouter = require("./components/Announcement/announcement.api")
-
 const questionRouter = require("./components/Question/question.api")
-
 const postRouter = require("./components/Post/post.api")
-
 const commentRouter = require("./components/Comment/comment.api")
 
 
