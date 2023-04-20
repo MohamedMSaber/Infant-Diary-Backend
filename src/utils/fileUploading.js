3a336467400ea1c680d23accb1ee8027f7e054d8
Mohamed Mostafa <Mohamedmsaber@outlook.com>
2023-04-20 05:39:59 +0200

    delete non useful comments

--- src/utils/fileUploading.js
+++ src/utils/fileUploading.js
@@ -2,16 +2,7 @@ const multer = require('multer');
 const AppError = require('./AppError');
 
 let options = (folderName)=>{
-    const storage = multer.diskStorage({}
-    //     destination: function (req, file, cb) {
-    //         cb(null, `Uploads/${folderName}`)
-    //     },
-    //     filename: function (req, file, cb) {
-    //         const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
-    //         cb(null, uniqueSuffix + '-' + file.originalname)
-    //     }
-    
-    )
+    const storage = multer.diskStorage({})
     function fileFilter(req, file, cb) {
         if (file.mimetype.startsWith('image')) {
             cb(null, true)
