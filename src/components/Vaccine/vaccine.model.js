c6f3bf43d03c9f02b802d4bd676834f1e7ceac1c
amirafathy-59 <amirafa592001@gmail.com>
2023-05-10 14:14:57 +0100

    update vaccine compulsory

--- src/components/Vaccine/vaccine.model.js
+++ src/components/Vaccine/vaccine.model.js
@@ -3,7 +3,7 @@ const bcrypt = require('bcrypt');
 
 const schema = Schema({
     name: { type: String, required: [true, 'name is required']},
-    compulsory:{type: String, required:[true, 'you must mention if it is compulsory or not'], enum:['Yes', 'No']},
+    compulsory:{type: String, required:[true, 'you must mention if it is compulsory or not'], enum:['Mandatory', 'Elective']},
     dose:{type: Number, required:[true, 'dose is required']},
     age:{type: Number, required:[true, 'age is required']},
     sideEffects:{type: String, required:[true, 'side effects is required']},
