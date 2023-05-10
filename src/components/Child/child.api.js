1ee976f16a891de90e2cd63d6fa9e5d510235784
amirafathy-59 <amirafa592001@gmail.com>
2023-05-10 14:38:36 +0100

    Update Child APIs

--- src/components/Child/child.api.js
+++ src/components/Child/child.api.js
@@ -3,14 +3,8 @@ const { addChild, updateChild, deleteChild, getChildren, getChild } = require(".
 const router=require('express').Router();
 
 
-router.route('/:userType/addChild').post(ProtectedRoutes,AllowedTo('parent'),addChild );
-
-router.route('/:id/:userType/updateChild').post(ProtectedRoutes,AllowedTo('parent'),updateChild );
-
-router.route('/:id/:userType/deleteChild').post(ProtectedRoutes,AllowedTo('parent'),deleteChild );
-
-router.route('/:id/:userType/getChild').post(ProtectedRoutes,AllowedTo('parent'),getChild );
-
-router.route('/:userType/getChildren').post(ProtectedRoutes,AllowedTo('parent'),getChildren );
+router.route('/:userType').post(ProtectedRoutes,AllowedTo('parent'),addChild );
+router.route('').get(getChildren );
+router.route('/:id').put(updateChild).get(getChild).delete(deleteChild);
 
 module.exports= router
