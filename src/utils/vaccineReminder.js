5334f36bf4d522dd2d33eb0fcea7343637ea17da
Mohamed Mostafa Saber <mohamedmsaber@outlook.com>
2023-05-27 19:10:04 +0300

    update vaccine reminder time

--- src/utils/vaccineReminder.js
+++ src/utils/vaccineReminder.js
@@ -29,7 +29,7 @@ async function vaccineReminder() {
 
 
 const sendVaccineReminder = () => {
-    schedule.scheduleJob('0 19 * * *', async () => {
+    schedule.scheduleJob('0 17 * * *', async () => {
       console.log('Sending vaccine Reminders...');
       await vaccineReminder();
     });
