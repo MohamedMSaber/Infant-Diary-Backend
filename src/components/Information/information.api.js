1d78025ac5e1643c3cde49ad943c784d0e61fcf3
amirafathy-59 <amirafa592001@gmail.com>
2023-07-07 10:54:14 +0100

    Update Information Api

--- src/components/Information/information.api.js
+++ src/components/Information/information.api.js
@@ -6,7 +6,7 @@ const router=require('express').Router();
 
 router.route('/').post(ProtectedRoutes,AllowedTo('admin'),uploadingSingleFile('image'),createInformation)
                  .get(ProtectedRoutes,AllowedTo('admin' , 'parent' ,'doctor', 'hospital'),getAllInformation);
-router.route('/:childId').get(ProtectedRoutes,AllowedTo('parent'),getInformation);
+router.route('/child/:childId').get(ProtectedRoutes,AllowedTo('parent'),getInformation);
 router.route('/:id').put(ProtectedRoutes,AllowedTo('admin'),uploadingSingleFile('image'),updateInformation)
                     .delete(ProtectedRoutes,AllowedTo('admin'),deleteInformation)
                     .get(ProtectedRoutes,AllowedTo('parent', 'admin'),getSpecificInformation);
